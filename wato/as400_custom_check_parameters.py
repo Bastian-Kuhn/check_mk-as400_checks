90d5f7bc08e9bb6b5738032c91294c5ccbc8269c
Colombo Simone <simone.colombo@mis.mediobanca.com>
2015-07-08 16:43:07 +0200

    Implementazione iniziale check su CPU e numero jobs

---
+++ wato/as400_custom_check_parameters.py
@@ -0,0 +1,32 @@
+register_check_parameters(
+    _("Operating System Resources"),
+    "as400",
+    _("AS/400 CPU Usage and Jobs Levels"),
+    Dictionary(
+        elements = [
+            ("cpu_levels",
+                Tuple(
+                    title = _("Levels for cpu usage (%)"),
+                    label = _("Levels for cpu usage (%)"),
+                    elements = [
+                        Percentage(title = _("Warning at:" ), maxvalue = 1500.0),
+                        Percentage(title = _("Critical at:"), maxvalue = 1500.0),
+                    ]
+                ),
+            ),
+            ("jobs_levels",
+                Tuple(
+                    title = _("Levels for Jobs"),
+                    label = _("Levels for Jobs"),
+                    elements = [
+                        Integer(title = _("Warning at:" ), maxvalue = 1000000),
+                        Integer(title = _("Critical at:"), maxvalue = 1000000),
+                    ]
+                ),
+            ),
+        ],
+    ),
+    '',
+    "dict"
+)
+
