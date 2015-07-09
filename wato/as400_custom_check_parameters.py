68562c7cb71b91811d6c7d1df229f5fd81ddd474
Colombo Simone <simone.colombo@mis.mediobanca.com>
2015-07-09 11:33:55 +0200

    Separata la gestione dei parametri dei jobs e delle cpu

--- wato/as400_custom_check_parameters.py
+++ wato/as400_custom_check_parameters.py
@@ -1,31 +1,48 @@
 register_check_parameters(
     _("Operating System Resources"),
-    "as400",
-    _("AS/400 CPU Usage and Jobs Levels"),
+    "as400_cpu",
+    _("AS/400 CPU Usage Levels"),
     Dictionary(
+        title = _("CPU Parameters"),
+        optional_keys = ["average"],
         elements = [
-            ("cpu_levels",
-                Dictionary(
-                    elements= [
-                        (
-                            "levels",
-                            Tuple(
-                                title = _("Levels for cpu usage (%)"),
-                                label = _("Levels for cpu usage (%)"),
-                                elements = [
-                                    Percentage(title = _("Warning at:" ), maxvalue = 1500.0),
-                                    Percentage(title = _("Critical at:"), maxvalue = 1500.0),
-                                ]
-                            )
+            ( "levels",
+                Tuple (
+                    title = _("Levels for cpu usage (%)"),
+                    elements = [
+                        Percentage(
+                            title = _("Warning at:" ),
+                            maxvalue = 1500.0,
+                            unit = "percentage"
                         ),
-                        (
-                            "average",
-                            Integer(title = _("Averaging on:"))
-                        )
+                        Percentage(
+                            title = _("Critical at:"),
+                            maxvalue = 1500.0,
+                            unit = "percentage"
+                        ),
                     ]
                 )
             ),
-            ("jobs_levels",
+            ( "average",
+                Integer(
+                    title = _("Averaging on:"),
+                    maxvalue = 144,
+                    unit = "minutes",
+                    default_value = 15
+                )
+            )
+        ]
+    ),
+    None,"dict"
+)
+
+register_check_parameters(
+    _("Operating System Resources"),
+    "as400_jobs",
+    _("AS/400 Job queue counters"),
+    Dictionary(
+        elements = [
+            ("job_levels",
                 Tuple(
                     title = _("Levels for Jobs"),
                     label = _("Levels for Jobs"),
@@ -33,11 +50,10 @@ register_check_parameters(
                         Integer(title = _("Warning at:" ), maxvalue = 1000000),
                         Integer(title = _("Critical at:"), maxvalue = 1000000),
                     ]
-                ),
-            ),
-        ],
+                )
+            )
+        ]
     ),
-    '',
-    "dict"
+    None,"dict"
 )
 
